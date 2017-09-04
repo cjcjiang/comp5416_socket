7f79b76a8ffb9662b7861165723ef6bb39b63565
Yuming JIANG <cjcjiang@gmail.com>
2017-09-04 16:43:10 +1000

    feat: q2 finish

--- q2/JiangYumingServer.py
+++ q2/JiangYumingServer.py
@@ -1,52 +1,21 @@
 from socket import *
-from threading import *
+import thread
 
-serverPort = 12010
-serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind(('', serverPort))
 
-class ThreadedServer(object):
-    def __init__(self, host, port):
-        self.host = host
-        self.port = port
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.sock.bind((self.host, self.port))
+def handler(connectionSocket, addr):
+    while 1:
+        sentence = connectionSocket.recv(1024)
+        capitalizedSentence = sentence.upper()
+        connectionSocket.send(capitalizedSentence)
 
-    def listen(self):
-        self.sock.listen(5)
-        while True:
-            client, address = self.sock.accept()
-            client.settimeout(60)
-            threading.Thread(target = self.listenToClient,args = (client,address)).start()
-
-    def listenToClient(self, client, address):
-        size = 1024
-        while True:
-            try:
-                data = client.recv(size)
-                if data:
-                    # Set the response to echo back the recieved data
-                    response = data
-                    client.send(response)
-                else:
-                    raise error('Client disconnected')
-            except:
-                client.close()
-                return False
-
-serverSocket.listen(1)
-print 'The server is ready to receive'
-while 1:
-    connectionSocket, addr = serverSocket.accept()
-
-    sentence = connectionSocket.recv(1024)
-
-    print sentence
-
-    capitalizedSentence = sentence.upper()
-
-    print capitalizedSentence
-
-    connectionSocket.send(capitalizedSentence)
-    connectionSocket.close()
+if __name__ == '__main__':
+    serverPort = 12010
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+    serverSocket.bind(('', serverPort))
+    serverSocket.listen(5)
+    print 'The server is ready to receive'
+    while 1:
+        print 'New thread waiting for connection, listening on port', serverPort
+        connectionSocket, addr = serverSocket.accept()
+        print 'Connected from:', addr
+        thread.start_new_thread(handler, (connectionSocket, addr))
